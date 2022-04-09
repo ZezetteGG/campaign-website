db6a9c039618fd3edcec96d6d3be2fa72ad18ff8
shera <omgpink75@yahoo.com>
2022-04-09 17:55:57 -0400

    Fixed trusted origins.

--- learning_log/settings.py
+++ learning_log/settings.py
@@ -110,7 +110,7 @@ AUTH_PASSWORD_VALIDATORS = [
 ]
 
 # Set trusted origins
-CSRF_TRUSTED_ORIGINS = ["https://curse-of-strahd.herokuapp.com/"]
+CSRF_TRUSTED_ORIGINS = ["https://curse-of-strahd.herokuapp.com",]
 
 # Internationalization
 # https://docs.djangoproject.com/en/4.0/topics/i18n/
